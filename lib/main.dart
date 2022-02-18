112576e5212ed5be381b4394cd494ececf182870
Alicelavander <alicelavander.dev@gmail.com>
2022-02-18 19:23:32 +0900

    revise initialize

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +12,6 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
     return const MaterialApp(
       title: 'smi',
       home: Login(),
