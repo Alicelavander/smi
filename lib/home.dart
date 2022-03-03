0e0f9cf7df71ae60e9aa7b9d164d30e2396dd854
Alicelavander <alicelavander.dev@gmail.com>
2022-03-03 21:50:14 +0900

    wip BottomNavigationBar

--- lib/home.dart
+++ lib/home.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'login.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   final String userId;
   const Home({Key? key, required this.userId}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<StatefulWidget> createState() => _Home();
+}
 
+class _Home extends State<Home> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("smi"),
@@ -32,8 +43,7 @@ class Home extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             //_buildAccountInfo(),
-            const Text('Hi! Start by posting your identity.', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            Text(userId),
+            const Text("Hi! Start by posting your identity.")
           ],
         ),
       ),
@@ -67,6 +77,20 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.feed)),
+          BottomNavigationBarItem(icon: Icon(Icons.groups)),
+        ],
+        currentIndex: _currentIndex,
+        fixedColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+
+
+      ),
+
     );
   }
 }
+
