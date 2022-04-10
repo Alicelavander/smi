4a36465a52ccc3dd5f145f2ab3a72ec62f2016be
Alicelavander <alicelavander.dev@gmail.com>
2022-04-11 00:55:54 +0900

    implemented feed

---
+++ lib/community/addexperience.dart
@@ -0,0 +1,114 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:smi/community/communityhome.dart';
+
+class AddExperience extends StatefulWidget {
+  final String communityId;
+  const AddExperience({Key? key, required this.communityId}) : super(key: key);
+  @override
+  _AddExperiencePage createState() => _AddExperiencePage();
+}
+
+class _AddExperiencePage extends State<AddExperience> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  String identityName = "";
+  bool _postAnonymous = true;
+
+  Future<void> addIdentity() async {
+    CollectionReference collection = db.collection('communities').doc(widget.communityId).collection('identities');
+    Query query = collection.where("name", isEqualTo: identityName);
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+
+    var result = await query.get();
+    if(result.docs.isEmpty){
+      collection.add({
+        'name': identityName
+      }).then((DocumentReference newDocument) => {
+        if(_postAnonymous){
+          collection.doc(newDocument.id).collection('population').add({
+            'name': 'Anonymous',
+          })
+        } else {
+          collection.doc(newDocument.id).collection('population').add({
+            'name': user?.displayName,
+          })
+        }
+      });
+    } else {
+      if(_postAnonymous){
+        collection.doc(result.docs.single.id).collection('population').add({
+          'name': 'Anonymous'
+        });
+      } else {
+        collection.doc(result.docs.single.id).collection('population').add({
+          'name': user?.displayName
+        });
+      }
+    }
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CommunityHome(communityId: widget.communityId),
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Add your identity")
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding:  const EdgeInsets.fromLTRB(25.0, 0, 25.0, 10.0),
+              child:TextFormField(
+                maxLengthEnforcement: MaxLengthEnforcement.none, decoration: const InputDecoration(
+                  labelText: "Enter your identity"
+              ),
+                onChanged: (String value) {
+                  identityName = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 0.0, 10.0),
+              child: SwitchListTile(
+                title: const Text('post anonymously?'),
+                value: _postAnonymous,
+                onChanged: (bool value){
+                  setState(() {
+                    _postAnonymous = value;
+                  });
+                },
+              ),
+            ),
+            ButtonTheme(
+              minWidth: 350.0,
+              // height: 100.0,
+              child: ElevatedButton(
+                onPressed: addIdentity,
+                child: const Text('Add',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, //ボタンの背景色
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/home/feed.dart
+++ lib/home/feed.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../community/communityhome.dart';
 
 class Feed extends StatefulWidget {
   const Feed({Key? key}) : super(key: key);
@@ -7,6 +11,20 @@ class Feed extends StatefulWidget {
 }
 
 class _FeedPage extends State<Feed> {
+  User? user = FirebaseAuth.instance.currentUser;
+  final db = FirebaseFirestore.instance;
+
+  Future<QuerySnapshot<Map<String, dynamic>>?> getPostsData() async {
+    Query query = db.collection('user-community-link').where("user", isEqualTo: user?.uid);
+    var result = await query.get();
+    QuerySnapshot<Map<String, dynamic>>? postList;
+    Future.wait(result.docs.map((document) async {
+      var post = await db.collection('posts').where("community", isEqualTo: document.id).get();
+      postList?.docs.add(post.docs.first);
+    }));
+    return postList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +32,41 @@ class _FeedPage extends State<Feed> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-
+            FutureBuilder<QuerySnapshot<Map<String, dynamic>>?> (
+              future: getPostsData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView(
+                    children: snapshot.data!.docs.map((document) {
+                      return Card(
+                        child: ListTile(
+                          title: Text(document["experience"]),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CommunityHome(communityId: document.id),
+                                )
+                            );
+                          },
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        elevation: 10,
+                        margin: const EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                      );
+                    }).toList(),
+                  );
+                } else {
+                  const Text("Start by joining or adding a community!", style: TextStyle(fontSize: 16));
+                }
+                // データが読込中の場合
+                return const Center(
+                  child: Text('Loading...'),
+                );
+              },
+            ),
           ],
         ),
       ),
